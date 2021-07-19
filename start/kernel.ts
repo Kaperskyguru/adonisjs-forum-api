cd61f7b6cfc470ee4d5f503207a1163ee0e74be2
Solomon Eseme <kaperskyguru@gmail.com>
2021-07-19 16:10:57 +0100

    Fix error

--- start/kernel.ts
+++ start/kernel.ts
@@ -20,7 +20,7 @@ import Server from "@ioc:Adonis/Core/Server";
 | are defined for every HTTP requests.
 |
 */
-Server.middleware.register(["Adonis/Core/BodyParserMiddleware"]);
+Server.middleware.register([() => import("@ioc:Adonis/Core/BodyParser")]);
 
 /*
 |--------------------------------------------------------------------------
