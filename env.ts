d8fd19b3ab1ebe1c1ac315f1dc5defcab1f73e13
Solomon Eseme <kaperskyguru@gmail.com>
2021-07-19 15:53:55 +0100

    Added Redis

--- env.ts
+++ env.ts
@@ -12,12 +12,13 @@
 |
 */
 
-import Env from '@ioc:Adonis/Core/Env'
+import Env from "@ioc:Adonis/Core/Env";
 
 export default Env.rules({
-	HOST: Env.schema.string({ format: 'host' }),
-	PORT: Env.schema.number(),
-	APP_KEY: Env.schema.string(),
-	APP_NAME: Env.schema.string(),
-	NODE_ENV: Env.schema.enum(['development', 'production', 'testing'] as const),
-})
+  HOST: Env.schema.string({ format: "host" }),
+  PORT: Env.schema.number(),
+  APP_KEY: Env.schema.string(),
+  APP_NAME: Env.schema.string(),
+  NODE_ENV: Env.schema.enum(["development", "production", "testing"] as const),
+  CACHE_DRIVER: Env.schema.string(),
+});
